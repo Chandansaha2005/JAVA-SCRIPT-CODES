ff350b6905ab58573786c8f4761a2040bf8b341c
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 14:48:15 +0530

    array things

--- arrays/array.js
+++ arrays/array.js
@@ -8,8 +8,9 @@ for (i = 0; i < n; i++) print(arr[i])
 
 print("first element = ", arr[0])
 print("last element = ", arr[4])
-arr2 = {
-    date: "20/02/25"
-    , time: "5.30"
-}
+arr2 = [4,"chandan",63.96,'p',true]
 print(arr2)
+arr2d=[[5,9,3],[7,4,5,8,9]]
+print(arr2d)
+brr=[4,"chandan",63.96,'p',true,[8,6,2,5,1]]
+print(brr)
