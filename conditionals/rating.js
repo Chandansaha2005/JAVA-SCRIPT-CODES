59dc565c5f8eed916bc71199e4793beada7ca47d
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 13:51:17 +0530

    conditionals examples

---
+++ conditionals/rating.js
@@ -0,0 +1,7 @@
+let print = console.log
+rate = 5
+if (rate == 1) print("Poor")
+else if (rate == 2) print("Below Average")
+else if (rate == 3) print("Average")
+else if (rate == 4) print("Good")
+else print("Exellent")
---
+++ conditionals/threeDigitNum.js
@@ -0,0 +1,4 @@
+let print=console.log
+x=455
+if(x>=100 && x<=999) print("Three Digit Number")
+else print("Not Three Digit Number")
