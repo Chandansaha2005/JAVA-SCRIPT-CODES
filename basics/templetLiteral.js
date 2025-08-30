7749880d10b64e0f9e8ace95ef9cc7ec9c0b8d3e
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 17:25:37 +0530

    templet literal

---
+++ basics/templetLiteral.js
@@ -0,0 +1,10 @@
+let print = console.log
+let age = 19
+let clg = "MCKV"
+print("My age is " + age + " my college name is " + clg)
+print("chandan")
+print('chandan')
+print(`chandan`)
+//print("chand"and")// can't print chand"an
+print(`chand"an`)
+print(`My age is ${age} my college name is ${clg}`)
