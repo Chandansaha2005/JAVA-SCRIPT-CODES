1286e18101e06027ca4ee86ca6936b43e03d89a4
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 12:52:52 +0530

    var let const done

--- basics/varLetConst.js
+++ basics/varLetConst.js
@@ -1,9 +1,14 @@
-var a=4;
+var a = 4;
 console.log(a)
-a=8
+a = 8
 console.log(a)
-const b=4;
+const b = 4;
 console.log(b)
 // b=8//error
 // console.log(b)
-let
+let c = 0// this is same as var but more preferable
+console.log(c)
+c = 60
+console.log(c)
+let clog = console.log
+clog("This is working")
