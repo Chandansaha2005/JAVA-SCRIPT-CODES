fc575f2804023a6579e3d622416c30be448d136a
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 23:12:33 +0530

    objects in java script

---
+++ objects/object.js
@@ -0,0 +1,22 @@
+// object is kind of like map/dictionary where we have "key-value" pairs
+var details = ["chandan", 19, 89.5, true]
+let print = console.log
+let x = {
+    "name": "chandan",
+    age: 19,
+    percentage: 89.5,
+    isSingle: true
+};
+
+print(x)
+print(x.name)
+print(x.age)
+print(x.percentage)
+print(x.isSingle)
+
+print(x)
+print(x["name"])
+print(x.age)
+print(x.percentage)
+print(x.isSingle)
+
