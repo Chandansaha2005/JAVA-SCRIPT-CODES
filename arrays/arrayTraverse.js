25e2003ca3d5522a02d34e5cfcfaaba5159e14fc
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 14:55:14 +0530

    for of loop

--- arrays/arrayTraverse.js
+++ arrays/arrayTraverse.js
@@ -1,5 +1,11 @@
+let print = console.log
 brr = [4, "chandan", 63.96, 'p', true, [8, 6, 2, 5, 1]]
 print(brr)
+//for loop
 for (let i = 0; i < brr.length; i++) {
     print(i, brr[i])
+}
+//forOf loop
+for (const i of brr) {
+    print(i)
 }
