1ab115c691542ebe422460a6aca491187ee0aa0e
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 13:43:02 +0530

    if else conditionals

---
+++ conditionals/evenOdd.js
@@ -0,0 +1,3 @@
+num=5
+if(num%2==0) console.log("even")
+else console.log("Odd")
---
+++ conditionals/ifelse.js
@@ -0,0 +1,7 @@
+let print = console.log
+age = 74
+if (age < 70 && age > 14) {
+    print("adult");
+}
+else if (age > 70) print("Somoy ghoniye eseche")
+else print("child")
