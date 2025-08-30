bd5f66dda33a1e1b516bab48b87812d687cd8a22
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 14:22:33 +0530

    array basics

---
+++ arrays/array.js
@@ -0,0 +1,10 @@
+let print = console.log
+arr = [1, 5, 2, 6, 7];
+print(arr)
+n = arr.length;
+print(n)
+print("Traverse :-")
+for (i = 0; i < n; i++) print(arr[i])
+
+print("first element = ", arr[0])
+print("last element = ", arr[4])
