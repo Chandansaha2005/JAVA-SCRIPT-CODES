7f3e2e03accb4bf79fb975fecb04160a567affa2
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 14:32:51 +0530

    list in javascript

---
+++ arrays/dynamic_array.js
@@ -0,0 +1,13 @@
+//c++ : Vectors  java : arraylist
+//javascript : array e sob 
+let print = console.log
+arr = [4, 2, 5, 6];
+print(arr)
+arr.push(0)// add element to last
+print(arr)
+arr.pop()// remove element from last 
+print(arr)
+arr.unshift(1)// add element to last
+print(arr)
+arr.shift()// remove element from last 
+print(arr)
