3f6552dafd1ce680258753e6a80b521ff28bc6c0
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 14:50:42 +0530

    array traverse

--- arrays/array.js
+++ arrays/array.js
@@ -8,9 +8,12 @@ for (i = 0; i < n; i++) print(arr[i])
 
 print("first element = ", arr[0])
 print("last element = ", arr[4])
-arr2 = [4,"chandan",63.96,'p',true]
+arr2 = [4, "chandan", 63.96, 'p', true]
 print(arr2)
-arr2d=[[5,9,3],[7,4,5,8,9]]
+arr2d = [[5, 9, 3], [7, 4, 5, 8, 9]]
 print(arr2d)
-brr=[4,"chandan",63.96,'p',true,[8,6,2,5,1]]
+brr = [4, "chandan", 63.96, 'p', true, [8, 6, 2, 5, 1]]
 print(brr)
+for (let i = 0; i < brr.length; i++) {
+    print(i, brr[i])
+}
---
+++ arrays/arrayTraverse.js
@@ -0,0 +1,5 @@
+brr = [4, "chandan", 63.96, 'p', true, [8, 6, 2, 5, 1]]
+print(brr)
+for (let i = 0; i < brr.length; i++) {
+    print(i, brr[i])
+}
