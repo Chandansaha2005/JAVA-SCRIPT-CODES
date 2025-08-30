c4453f618b0fc275593d981bd4e48abfe16b6d16
Chandan Saha <chandansaha1945@gmail.com>
2025-08-30 13:57:22 +0530

    triple equal to

---
+++ conditionals/tripleEqual.js
@@ -0,0 +1,4 @@
+a = 5//number 
+b = '5'//string
+console.log(a == b)// will come ture because '==' checks only the value , Not the type 
+console.log(a === b)//// will come false because '===' checks the value and the type both
