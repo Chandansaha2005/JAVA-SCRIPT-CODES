bf099710f50abe36b4522fcecf4e733af1edbd86
Chandan Saha <chandansaha1945@gmail.com>
2025-08-31 23:20:32 +0530

    object attributes traverse with for in loop

--- objects/forin.js
+++ objects/forin.js
@@ -4,12 +4,14 @@ let chandan = {
     college: "MCKV",
     isSingle: true
 };
-let print=console.log
+let print = console.log
 print(chandan)
 
 for (const key in chandan) {
     if (Object.prototype.hasOwnProperty.call(chandan, key)) {
-        print(chandan[key])
-        
+        print(key, chandan[key])
     }
+}
+for (const key in chandan) {
+    print(key, chandan[key])
 }
