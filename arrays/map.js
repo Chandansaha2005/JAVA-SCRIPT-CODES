57d05a20307da6fae8c18b3a79ee56b4e1d71ad6
Chandan Saha <chandansaha1945@gmail.com>
2025-09-01 10:10:43 +0530

    mapping done

--- arrays/map.js
+++ arrays/map.js
@@ -7,10 +7,39 @@ let brr = []
 function double(n) {
     return n * 2;
 }
+brr = arr.map(double)
+
 function square(n) {
     return n * n;
 }
 brr = arr.map(square)
+
+function add10(n) {
+    return n + 10;
+}
+brr = arr.map(add10)
+
+brr = arr.map(
+    function mul5(n) {
+        return n * 5
+    }
+);
+
+brr = arr.map(
+    (n) => {
+        return n * 5
+    }
+);
+
+arr = arr.map(
+    (n) => {
+        return n * 5
+    }
+);
+
+arr = arr.map((n) => n * 100);
+
 let print = console.log
 print(brr)
+print(arr)
 
