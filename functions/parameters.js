145b3f0c5b08524c4b69558fd5133266b6de8350
Chandan Saha <chandansaha1945@gmail.com>
2026-05-18 22:11:58 +0530

    rest parameters

--- functions/parameters.js
+++ functions/parameters.js
@@ -12,3 +12,20 @@ let VS = (p1, p2) => {
 VS("pikachu","onex")
 VS("charizard","evee")
 
+//when we have multiple parameters , we need that much arguments
+// in this case we can use rest operator 
+
+let numbers=function(...num){
+    console.log(num)
+}
+
+numbers(1,2,3,4,5,6,7,8,9,10) // this will give an array of numbers 
+/*
+[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+*/
+
+let no=(a,b,c,...val)=>{
+    console.log(a,b,c ,val)
+}
+
+no(1,2,3,4,5,6,7,8,9) //1 2 3 [ 4, 5, 6, 7, 8, 9 ]
