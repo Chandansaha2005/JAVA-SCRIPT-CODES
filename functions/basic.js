a5ea9f7d7791ea7795db000004d9328cb3c2bec0
Chandan Saha <chandansaha1945@gmail.com>
2026-05-18 21:23:47 +0530

    func revision

---
+++ functions/basic.js
@@ -0,0 +1,15 @@
+function pikachu(){
+    console.log("Pika pika")
+}
+pikachu()
+
+let chikorita=function(){
+    console.log("chikko")
+}
+chikorita()
+
+let cannonbolt=()=>{ // fat arrow func
+    console.log("dum dum dum")
+}
+cannonbolt()
+
